22b86449f4651f3abb26c4cca90a0946fc9a3dcd
Marcus Camaroni <psminchia4153@hotmail.com>
2023-05-29 23:28:18 +1000

    pot working

--- src/sequence.c
+++ src/sequence.c
@@ -4,7 +4,7 @@
 #include "buzzer.h"
 #include "buttons.h"
 #include "timer.h"
-
+#include "delay.h"
 
 typedef enum
 {
@@ -20,9 +20,9 @@ typedef enum
 uint32_t seed = SID;
 
 extern volatile uint8_t pb_debounced;
+uint16_t duration;
 extern uint8_t digits[];
-uint8_t segs[] = {Spi_Off, Spi_Off};
-
+volatile uint8_t segs[] = {Spi_Off, Spi_Off};
 
 uint8_t generate_step(uint32_t *state)
 {
@@ -37,6 +37,14 @@ uint8_t generate_step(uint32_t *state)
     return *state & 0b11;
 }
 
+void display_score(uint16_t score)
+{
+    segs[0] = Spi_Off;
+    if (score > 9)
+        segs[0] = digits[score % 100 / 10];
+    segs[1] = digits[score % 10];
+}
+
 bool runSequence(uint16_t sequenceLength)
 {
     uint32_t lfsr_state = seed;
@@ -49,6 +57,7 @@ bool runSequence(uint16_t sequenceLength)
     uint8_t pb_sample_r = 0xFF;
     uint8_t pb_changed, pb_rising, pb_falling;
 
+    allow_updating_playback_delay = true;
     while (1)
     {
         pb_sample_r = pb_sample;
@@ -61,127 +70,149 @@ bool runSequence(uint16_t sequenceLength)
         switch (pb)
         {
         case UserInput:
+
+            elapsed_time = 0;
+            allow_updating_playback_delay = false;
+
+            tone_stop();
+
             segs[0] = Spi_Off;
             segs[1] = Spi_Off;
+
             if (pb_falling & PIN4_bm)
             {
                 pb = PB1;
-                play_tone(0);
-                //printf("PB1 Pressed\n");
+                // printf("PB1 Pressed\n");
             }
             else if (pb_falling & PIN5_bm)
             {
                 pb = PB2;
-                play_tone(1);
-                //printf("PB2 Pressed\n");
+                // printf("PB2 Pressed\n");
             }
             else if (pb_falling & PIN6_bm)
             {
                 pb = PB3;
-                play_tone(2);
-                //printf("PB3 Pressed\n");
+                // printf("PB3 Pressed\n");
             }
             else if (pb_falling & PIN7_bm)
             {
                 pb = PB4;
-                play_tone(3);
-                //printf("PB4 Pressed\n");
+                // printf("PB4 Pressed\n");
             }
             break;
         case PB1:
-            spi_write(SEG1);
+            play_tone(0);
+            segs[0] = SegLeft;
             if (pb_rising & PIN4_bm)
             {
                 tone_stop();
                 if (step == 0)
                 {
                     pb = Success;
-                    //printf("PB1 Correct\n");
+                    // printf("PB1 Correct\n");
                 }
                 else
                 {
                     pb = Fail;
-                    //printf("PB1 Incorrect\n");
+                    // printf("PB1 Incorrect\n");
                 }
             }
             break;
         case PB2:
-            spi_write(SEG2);
+            play_tone(1);
+            segs[0] = SegRight;
             if (pb_rising & PIN5_bm)
             {
                 tone_stop();
                 if (step == 1)
                 {
                     pb = Success;
-                    //printf("PB2 Correct\n");
+                    // printf("PB2 Correct\n");
                 }
                 else
                 {
                     pb = Fail;
-                    //printf("PB2 Incorrect\n");
+                    // printf("PB2 Incorrect\n");
                 }
             }
             break;
         case PB3:
-            spi_write(SEG3);
+            play_tone(2);
+            segs[1] = SegLeft;
             if (pb_rising & PIN6_bm)
             {
                 tone_stop();
                 if (step == 2)
                 {
                     pb = Success;
-                    //printf("PB3 Correct\n");
+                    // printf("PB3 Correct\n");
                 }
                 else
                 {
                     pb = Fail;
-                    //printf("PB3 Incorrect\n");
+                    // printf("PB3 Incorrect\n");
                 }
             }
             break;
         case PB4:
-            spi_write(SEG4);
+            play_tone(3);
+            segs[1] = SegRight;
             if (pb_rising & PIN7_bm)
             {
                 tone_stop();
                 if (step == 3)
                 {
                     pb = Success;
-                    //printf("PB4 Correct\n");
+                    // printf("PB4 Correct\n");
                 }
                 else
                 {
                     pb = Fail;
-                    //printf("PB4 Incorrect\n");
+                    // printf("PB4 Incorrect\n");
                 }
             }
             break;
         case Success:
+            count++;
             if (sequenceLength == count)
             {
+                printf("SUCCESS\n");
+                printf("%d\n", sequenceLength);
+
+                display_score(sequenceLength);
+                delay_ms(duration);
                 segs[0] = Spi_On;
                 segs[1] = Spi_On;
-                // delay
+                delay_ms(duration);
                 segs[0] = Spi_Off;
                 segs[1] = Spi_Off;
-                //printf("success state\n");
+                // printf("success state\n");
                 return true;
             }
             else
             {
-                count++;
                 step = generate_step(&lfsr_state);
                 pb = UserInput;
-                //printf("moving into delay state\n");
+                // printf("moving into delay state\n");
                 break;
             }
         case Fail:
+            printf("GAME OVER\n");
+            printf("%d\n", sequenceLength);
+
+            display_score(sequenceLength);
+            delay_ms(playback_time);
             segs[0] = Spi_Fail;
             segs[1] = Spi_Fail;
-            // delay
+            delay_ms(playback_time);
             segs[0] = Spi_Off;
             segs[1] = Spi_Off;
+
+            count++;
+            for (; count < sequenceLength; count++)
+                generate_step(&lfsr_state);
             seed = lfsr_state;
+
             return false;
 
         default:
@@ -195,23 +226,33 @@ bool runSequence(uint16_t sequenceLength)
 void generate_sequence(uint16_t sequenceLength)
 {
     uint32_t lfsr_state = seed;
-    static uint8_t sequencesSegs[] = {
-        SEG1, SEG2, SEG3, SEG4};
-    for (uint16_t i = 0; i <= sequenceLength; i++)
+    for (uint16_t i = 0; i < sequenceLength; i++)
     {
+        duration = get_duration();
         uint8_t step = generate_step(&lfsr_state);
         play_tone(step);
-        spi_write(sequencesSegs[step]);
+
+        switch (step)
+        {
+        case 0:
+            segs[0] = SEG1;
+            break;
+        case 1:
+            segs[0] = SEG2;
+            break;
+        case 2:
+            segs[1] = SEG3;
+            break;
+        case 3:
+            segs[1] = SEG4;
+            break;
+        }
+
+        delay_ms(duration >> 1);
+
+        tone_stop();
         segs[0] = Spi_Off;
         segs[1] = Spi_Off;
-        tone_stop();
+        delay_ms(duration >> 1);
     }
-}
-
-void display_score(uint16_t score)
-{
-    segs[0] = Spi_Off;
-    if (score > 9)
-        segs[0] = digits[score % 100 / 10];
-    segs[1] = digits[score % 10];
 }
